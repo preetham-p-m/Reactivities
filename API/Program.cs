d13432e5f456f1f341549a6902968481a9f81879
preetham-coupa <preetham.p-m@coupa.com>
2023-03-06 10:59:19 +0530

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using Persistence;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultSqlLite"));
+});
+
+//builder.Services.AddTransient<MySqlConnection>(_ =>
+//    new MySqlConnection(builder.Configuration.GetConnectionString("DefaultMySql")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    Task task = context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError("Error occured during migration!!!\n", ex);
+    throw;
+}
+
+app.Run();
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,10 @@
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Activity> Activities { get; set; }
+}
