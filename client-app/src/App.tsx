fe69ae2d7500dc54346d8d7c1d074ca410a4b4ae
preetham-coupa <preetham.p-m@coupa.com>
2023-04-10 09:47:59 +0530

    refactored client app code

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,28 +1,13 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import axios from "axios";
-import { Header } from "semantic-ui-react";
-import List from "semantic-ui-react/dist/commonjs/elements/List";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ActivityIndex from "./components/Activity/ActivityIndex";
 
 function App() {
-  const [activities, setActivities] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
-      setActivities(response.data);
-    });
-  }, []);
-
   return (
-    <div>
-      <Header as="h2" icon="users" content="Reactivities" />
-      <List>
-        {activities.map((activity: any) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ActivityIndex />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
-import "./index.css";
+import "./styles.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <App />
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
